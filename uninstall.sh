fae0fffbb39a5f7d0e3d604ff02506e3340cbda3
huangdy <coololdman@gmail.com>
2015-02-07 00:24:15 -0500

    make tomcat7 as service

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,14 @@
 
 echo ... uninstall the xChangeCore installation ... >> uninstall.log
 
-echo sudo /usr/lib/apache-tomcat-7.0.57/bin/shutdown.sh >> uninstall.log
-sudo /usr/lib/apache-tomcat-7.0.57/bin/shutdown.sh 2>&1 | tee -a uninstall.log
+echo sudo service tomcat7 stop >> uninstall.log
+sudo service tomcat7 stop 2>&1 | tee -a uninstall.log
 
-echo sudo rm -fr /usr/lib/apache-tomcat-7.0.57 >> uninstall.log
-sudo rm -fr /usr/lib/apache-tomcat-7.0.57 2>&1 | tee -a uninstall.log
+echo sudo rm -fr /usr/share/tomcat7 >> uninstall.log
+sudo rm -fr /usr/share/tomcat7 2>&1 | tee -a uninstall.log
+
+echo sudo rm /etc/init.d/tomcat7 >> uninstall.log
+sudo rm /etc/init.d/tomcat7 2>&1 | tee -a uninstall.log
 
 echo sudo service openfire stop >> uninstall.log
 sudo service openfire stop 2>&1 | tee -a uninstall.log
@@ -17,7 +20,8 @@ sudo dpkg -r openfire 2>&1 | tee -a uninstall.log
 echo sudo dpkg -P openfire >> uninstall.log
 sudo dpkg -P openfire 2>&1 | tee -a uninstall.log
 
-echo mysql -u root -p < openfire/conf/openfire_cleanup.sql >> uninstall.log
+echo 'mysql -u root -p < openfire/conf/openfire_cleanup.sql' >> uninstall.log
+echo 'Enter MySQL root password: '
 mysql -u root -p < openfire/conf/openfire_cleanup.sql 2>&1 | tee -a uninstall.log
 
 echo sudo /opt/opendj/bin/stop-ds >> uninstall.log
