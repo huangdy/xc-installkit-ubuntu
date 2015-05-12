0ade84b49dd816ef11e5323f0b6bd5e50a856873
huangdy <coololdman@gmail.com>
2015-05-12 08:44:02 -0400

    Added firewall rules for openfire

---
+++ openfire/firewall.sh
@@ -0,0 +1,11 @@
+#!/bi/sh
+
+sudo ufw allow 9090/tcp
+sudo ufw allow 9091/tcp
+sudo ufw allow 5222/tcp
+sudo ufw allow 7777/tcp
+sudo ufw allow 7443/tcp
+sudo ufw allow 7070/tcp
+sudo ufw allow 3478/tcp
+sudo ufw allow 3479/tcp
+
