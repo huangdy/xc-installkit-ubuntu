4e66e6381b259357d21548b6b3da22949f8bf9e5
huangda <huangda@gv56bz1-u1410.us-arl-trg.leidos.com>
2015-06-03 09:03:14 -0400

    Modified the password substitution for openfire.xml and opendj-rest2ldap.json

--- openfire/install.sh
+++ openfire/install.sh
@@ -10,32 +10,41 @@ sudo dpkg -i openfire_3.7.1_all.deb 2>&1 | tee -a ../install.log
 
 read -p "Enter the XMPP's FQDN: " fqdn
 
-echo create tables for OpenFire 2>&1 | tee -a ../install.log
+echo create user openfire@localhost and tables for OpenFire 2>&1 | tee -a ../install.log
 echo cp conf/openfire_mysql.sql.orig conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 cp conf/openfire_mysql.sql.orig conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
+
 echo rpl "%SYSADMINPASS%" "$sa_passwd" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 rpl "%SYSADMINPASS%" "$sa_passwd" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
+
 echo rpl "%ADMINPASS%" "$au_passwd" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 rpl "%ADMINPASS%" "$au_passwd" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
+
 echo rpl "%FQDN%" "$fqdn" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 rpl "%FQDN%" "$fqdn" conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 
 echo "mysql -u root -p < conf/openfire_mysql.sql" 2>&1 | tee -a ../install.log
 mysql -u root -p < conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
+
 echo rm conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 rm conf/openfire_mysql.sql 2>&1 | tee -a ../install.log
 
 echo configure the conf/openfire.xml 2>&1 | tee -a ../install.log
 echo cp conf/openfire.xml.orig conf/openfire.xml 2>&1 | tee -a ../install.log
 cp conf/openfire.xml.orig conf/openfire.xml 2>&1 | tee -a ../install.log
+
 echo rpl "%SYSADMINPASS%" "$sa_passwd" conf/openfire.xml 2>&1 | tee -a ../install.log
-rpl "%SYSADMINPASS%" "$sa_passwd" conf/openfire.xml 2>&1 | tee -a ../install.log
+rpl "%SYSADMINPASS%" $sa_passwd conf/openfire.xml 2>&1 | tee -a ../install.log
+
 echo sudo cp /etc/openfire/openfire.xml /etc/openfire/openfire.xml.orig 2>&1 | tee -a ../install.log
 sudo cp /etc/openfire/openfire.xml /etc/openfire/openfire.xml.orig 2>&1 | tee -a ../install.log
+
 echo sudo chown openfire:openfire /etc/openfire/openfire.xml.orig 2>&1 | tee -a ../install.log
 sudo chown openfire:openfire /etc/openfire/openfire.xml.orig 2>&1 | tee -a ../install.log
+
 echo sudo cp conf/openfire.xml /etc/openfire 2>&1 | tee -a ../install.log
 sudo cp conf/openfire.xml /etc/openfire 2>&1 | tee -a ../install.log
+
 echo rm conf/openfire.xml 2>&1 | tee -a ../install.log
 rm conf/openfire.xml 2>&1 | tee -a ../install.log
 
@@ -45,8 +54,10 @@ cp /etc/openfire/security/keystore security 2>&1 ../install.log
 
 echo cp installKeystore.orig installKeystore 2>&1 | tee -a ../install.log
 cp installKeystore.orig installKeystore 2>&1 | tee -a ../install.log
+
 echo rpl "%HOSTNAME%" "$fqdn" installKeystore 2>&1 | tee -a ../install.log
 rpl "%HOSTNAME%" "$fqdn" installKeystore 2>&1 | tee -a ../install.log
+
 echo rpl "%KEYSTORE_PASS%" "$sa_passwd" installKeystore 2>&1 | tee -a ../install.log
 rpl "%KEYSTORE_PASS%" "$sa_passwd" installKeystore 2>&1 | tee -a ../install.log
 
@@ -55,12 +66,16 @@ echo Generate the keystore and keystore.jks 2>&1 | tee -a ../install.log
 
 echo sudo cp /etc/openfire/security/keystore /etc/openfire/security/keystore.orig 2>&1 | tee -a ../install.log
 sudo cp /etc/openfire/security/keystore /etc/openfire/security/keystore.orig 2>&1 | tee -a ../install.log
+
 echo sudo cp security/keystore /etc/openfire/security 2>&1 | tee -a ../install.log
 sudo cp security/keystore /etc/openfire/security 2>&1 | tee -a ../install.log
+
 echo sudo rm security/keystore 2>&1 | tee -a ../install.log
 sudo rm security/keystore 2>&1 | tee -a ../install.log
+
 echo sudo chown openfire:openfire /etc/openfire/security/keystore.orig 2>&1 | tee -a ../install.log
 sudo chown openfire:openfire /etc/openfire/security/keystore.orig 2>&1 | tee -a ../install.log
+
 echo rm installKeystore 2>&1 | tee -a ../install.log
 rm installKeystore 2>&1 | tee -a ../install.log
 
@@ -87,6 +102,7 @@ rpl "%FQDN%" "$fqdn" ../xchangecore/xchangecore/core.properties 2>&1 | tee -a ..
 
 echo sudo mv ../xchangecore/xchangecore /usr/share/tomcat7/webapps 2>&1 | tee -a ../install.log
 sudo mv ../xchangecore/xchangecore /usr/share/tomcat7/webapps 2>&1 | tee -a ../install.log
+
 echo sudo mv ../xchangecore/opendj-connector /usr/share/tomcat7/webapps 2>&1 | tee -a ../install.log
 sudo mv ../xchangecore/opendj-connector /usr/share/tomcat7/webapps 2>&1 | tee -a ../install.log
 
--- xchangecore/install.sh
+++ xchangecore/install.sh
@@ -19,17 +19,23 @@ mkdir xchangecore && cd xchangecore && jar xvf ../xchangecore-2.0.0.war 2>&1 | t
 
 cd $basedir
 echo rpl -q "%LOCATION%" "$lat_lon" xchangecore/core.properties 2>&1 | tee -a ../../install.log
-rpl -q "%LOCATION%" "$lat_lon" xchangecore/core.properties 2>&1 | tee -a ../../install.log
+rpl -q "%LOCATION%" $lat_lon xchangecore/core.properties 2>&1 | tee -a ../../install.log
 
 echo cp conf/xchangecore_creation.sql.orig conf/xchangecore_creation.sql 2>&1 | tee -a ../../install.log
 cp conf/xchangecore_creation.sql.orig conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
-echo rpl "%SYSADMINPASS%" "$sa_passwd" conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
 
-rpl "%SYSADMINPASS%" "$sa_passwd" conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
+echo rpl "%SYSADMINPASS%" "$sa_passwd" conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
+rpl "%SYSADMINPASS%" $sa_passwd conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
+
 echo "mysql -u root -p < conf/xchangecore_creation.sql" 2>&1 | tee -a ../install.log
 mysql -u root -p < conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
+
 echo rm conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
 rm conf/xchangecore_creation.sql 2>&1 | tee -a ../install.log
 
 echo tar xvf opendj-connector.war 2>&1 | tee -a ../install.log
 tar xvf opendj-connector.war 2>&1 | tee -a ../install.log
+
+echo rpl "%SYSADMINPASS%" $sa_passwd opendj-connector/opendj-rest2ldap-servlet.json 2>&1 | tee -a ../install.log
+rpl "%SYSADMINPASS%" $sa_passwd opendj-connector/opendj-rest2ldap-servlet.json 2>&1 | tee -a ../install.log
+
